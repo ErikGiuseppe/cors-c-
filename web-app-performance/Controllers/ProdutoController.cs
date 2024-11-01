613c104af9a40c92367a8e36e8fe901a1359d295
ErikGiuseppe <erikkato2004@gmail.com>
2024-10-31 21:38:01 -0300

    colocando o rabbit

--- web-app-performance/Controllers/ProdutoController.cs
+++ web-app-performance/Controllers/ProdutoController.cs
@@ -1,11 +1,16 @@
 ﻿using Dapper;
+using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualBasic;
 using MySqlConnector;
 using Newtonsoft.Json;
 using StackExchange.Redis;
+using System.Text;
 using web_app_domain;
 using web_app_repository;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace web_app_performance.Controllers
 {
@@ -74,6 +79,59 @@ namespace web_app_performance.Controllers
             await db.KeyDeleteAsync(key);
 
             return Ok();
+        }
+        [HttpPost("/produtor")]
+        public async Task<IActionResult> Produtor([FromBody] Produto produto) 
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            const string fila = "produto_cadastrado";
+            channel.QueueDeclare(queue: fila, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+           
+            var mensagemJson = JsonConvert.SerializeObject(produto);
+            var body = Encoding.UTF8.GetBytes(mensagemJson);
+            channel.BasicPublish(exchange: "", routingKey: fila, basicProperties: null, body: body);
+            return Ok();
+
+        }
+        [HttpGet("/consumer")]
+        public async Task<IActionResult> Consumer()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            const string fila = "produto_cadastrado";
+            channel.QueueDeclare(queue: fila, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, args) =>
+            {
+                var body = args.Body.ToArray();
+                var mensagem = Encoding.UTF8.GetString(body);
+
+                var produto = JsonConvert.DeserializeObject<Produto>(mensagem);
+
+                if (produto != null)
+                {
+              
+                    Console.WriteLine($"Produto cadastrado: {produto.Nome}, Quantidade Inicial: {produto.QuantidadeEstoque}");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Produto inválido recebido.");
+                }
+            };
+            channel.BasicConsume(queue: fila, autoAck: true, consumer: consumer);
+            return Ok();
+
         }
     }
 }
